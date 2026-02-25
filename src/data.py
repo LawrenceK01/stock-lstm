2e3db85956c55169a6389b6b21a6f67b9f5a448f
LawrenceK01 <lawrencekenworthy@gmail.com>
2026-02-25 16:47:02 +0000

    Initial commit 25/2/26

--- src/data.py
+++ src/data.py
@@ -1,16 +1,23 @@
 import pandas as pd
+import numpy as np
 
 def load_data():
-    df = pd.read_csv("data/portfolio_data.csv")
+    df = pd.read_csv("../data/portfolio_data.csv")
     df["Date"] = pd.to_datetime(df["Date"])
+    df = df.sort_values("Date")
+    df = df.set_index("Date")
     return df
 
 def compute_returns(df):
     stock_cols = ["AMZN", "DPZ", "BTC", "NFLX"]
     return df[stock_cols].pct_change()
 
-# Scale asset values between -1 and 1
-from sklearn.preprocessing import StandardScaler
+def create_sequences(data, window_size=30):
+    X, y = [], []
+    for i in range(len(data) - window_size):
+        X.append(data[i:i+window_size])
+        y.append(data[i+window_size])
+    return np.array(X), np.array(y)
 
 
 
